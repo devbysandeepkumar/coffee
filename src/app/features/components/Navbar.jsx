6e2e21c0e8c0a4a99cdfe3a670d6f443c78cfc97
devbysandeepkumar <devbysandeepkumar@gmail.com>
2026-08-04 14:53:39 +0530

    Some fixes in navbar

--- src/app/features/components/Navbar.jsx
+++ src/app/features/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { Home, Coffee, Heart, Bag2, User, Setting2 } from "iconsax-reactjs"
 const Navbar = () => {
   return (
-    <main className="w-44 h-100 fixed right-15 top-5 bg-[#1B222C] px-5 py-6 border border-[#9CA3AD] rounded-lg transform transition ease-in duration-500 overflow-hidden text-white">
-      <ul className="flex flex-col gap-7 font-medium">
+    <main className="w-44 h-80 fixed right-18 top-5 bg-[#1B222C] px-5 py-6 border border-[#9CA3AD] rounded-lg transform transition ease-in duration-500 overflow-hidden text-white">
+      <ul className="flex flex-col gap-6 font-medium">
         <li className="flex items-center cursor-pointer gap-3"><Home /><span>Home</span></li>
         <li className="flex items-center cursor-pointer gap-3"><Coffee /><span>Recipe</span></li>
         <li className="flex items-center cursor-pointer gap-3"><Heart /><span>Favorite</span></li>
