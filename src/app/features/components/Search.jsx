afeb4fc2bd302d25c920d13e6a132c1f975d3db8
devbysandeepkumar <devbysandeepkumar@gmail.com>
2026-08-12 14:35:12 +0530

    fixing some glitches in searchbar

--- src/app/features/components/Search.jsx
+++ src/app/features/components/Search.jsx
@@ -1,6 +1,6 @@
 const Search = ({value, onChange}) => {
   return (
-    <main className="w-1/2 md:w-1/3 lg:w-1/3">
+    <main className="px-4 md:w-1/3 lg:w-1/3">
       <div className="w-full h-8 rounded-xl px-2 flex justify-center items-center bg-amber-300">
         <input className="w-full h-full outline-none" type="text" value={value} onChange={onChange} placeholder="Enter name of coffee."/>
       </div>
