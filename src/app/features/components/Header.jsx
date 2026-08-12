bf7defcbe185bd7ce7e21447316700140e5dbdae
devbysandeepkumar <devbysandeepkumar@gmail.com>
2026-08-12 14:40:01 +0530

    fixing some glitches in searchbar

--- src/app/features/components/Header.jsx
+++ src/app/features/components/Header.jsx
@@ -32,10 +32,10 @@ const Header = () => {
           />
 
           {searches.length > 0 && (
-            <div className="absolute top-full mt-2 w-1/3">
+            <div className="absolute top-full mt-2 px-4 md:w-1/3 lg:w-1/3">
               {searches.map((item) => (
                 <div key={item.id}  onClick={()=>navigate(`/recipe/${item?.id}`)} className="px-4 py-2 cursor-pointer">
-                  <h1>
+                  <h1 className="font-[Poppins] font-medium md:font-semibold lg:font-semibold text-sm text-[#9CA3AD]">
                     {item?.title} {item?.subtitle}
                   </h1>
                 </div>
